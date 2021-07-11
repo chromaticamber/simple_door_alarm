067bb4c4f447152cbafefdd2ce0eae0b2e35b9d0
Amber <86908677+chromaticamber@users.noreply.github.com>
2021-07-11 20:28:34 +0800

    add sleeping time

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.magneticForce(Dimension.Strength))
+})
 basic.forever(function () {
-    if (input.magneticForce(Dimension.Strength) > 200) {
+    if (input.magneticForce(Dimension.Strength) < 300) {
         basic.showIcon(IconNames.Confused)
         soundExpression.giggle.play()
         basic.pause(2000)
         basic.clearScreen()
     }
+    basic.pause(500)
 })
